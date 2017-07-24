1fc2ac83c089453d9f4ce80c1df21ab830b9225f
emtees0 <matbaldyga@gmail.com>
2017-07-24 19:20:22 +0200

    deploy

---
+++ app.deploy.js
@@ -0,0 +1,58 @@
+var express = require('express'),
+    app = express(),
+    cookieSession = require('cookie-session'),
+
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    flash = require('connect-flash'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    passportLocalMongoose = require('passport-local-mongoose'),
+
+    User = require('./models/user'),
+
+    seedDB = require('./seed');
+
+var authRoutes = require('./routes/auth'),
+    indexRoutes = require('./routes/index'),
+    proceduresRoutes = require('./routes/procedures');
+
+mongoose.connect('mongodb://admin:Admin47@ds141232.mlab.com:41232/skinguide');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public')),
+app.use(flash());
+
+// seedDB()
+
+//==========
+// PASSPORT CONFIG
+//==========
+app.use(cookieSession({
+  name: 'session',
+  keys: ['gjeoisgf49ds489rv4ds489'],
+  maxAge: 48*60*60*1000,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//passes currentUser to every template
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use(authRoutes);
+app.use(indexRoutes);
+app.use(proceduresRoutes);
+
+app.listen(3000, function() {
+  console.log('The SkinGuide Server Has Started!');
+});
---
+++ config/webpack.dev.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './public/js/script.js',
+  ],
+  output: {
+    path: '/',
+    filename: 'script-es6.js',
+    publicPath: 'http://localhost:3000/scripts/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+  //     {
+  //       test: /\.css$/,
+  //       use: [ 'style-loader', 'css-loader' ]
+  //     },
+  //     {
+  //     test: /\.scss$/,
+  //     use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+  //     },
+  //     {
+  //     test: /\.(png|jpg|gif)$/,
+  //     use: [
+  //         {
+  //           loader: 'url-loader',
+  //           options: {
+  //             limit: 8192
+  //           }
+  //         }
+  //       ]
+  //     }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      // beautify: true,
+      minimize: true,
+      comments: false,
+    }),
+  ],
+};
