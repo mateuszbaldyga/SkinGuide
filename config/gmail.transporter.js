ed95c279928ba92cd3785d89d465238d17593ad6
emtees0 <matbaldyga@gmail.com>
2017-08-22 18:27:41 +0200

    repo clean

---
+++ config/gmail.transporter.js
@@ -0,0 +1,18 @@
+var nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    type: 'OAuth2',
+    user: 'moja.web.aplikacja@gmail.com',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: process.env.ACCESS_TOKEN,
+    expires: 3600
+  }
+});
+
+module.exports = transporter;
