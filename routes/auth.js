8139a61c5d2d459996c0eef50af8b540e5d40b1e
emtees0 <matbaldyga@gmail.com>
2017-08-24 00:43:54 +0200

    sign form layout change

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,21 @@
 var express = require('express'),
     router = express.Router(),
     passport = require('passport'),
+    passwordValidator = require('password-validator'),
+    usernameSchema = new passwordValidator(),
+    passwordSchema = new passwordValidator(),
 
     User = require('../models/user');
+
+    usernameSchema
+      .is().max(15)
+      .has().not().spaces();
+
+    passwordSchema
+      .is().min(6)
+      .has().lowercase()
+      .has().digits()
+      .has().not().spaces();
 
 //REGISTER
 router.get('/register', function(req, res) {
@@ -11,19 +24,33 @@ router.get('/register', function(req, res) {
 
 router.post('/register', function(req, res) {
   if(req.body.password === req.body.confirmPass) {
-    var newUser = new User({
-      username: req.body.username
-    });
-    User.register(newUser, req.body.password, function(err, user) {
-      if (err) {
-        console.log(err);
-        req.flash('error', 'Podana nazwa użytkownika istnieje.');
-        res.redirect('/register');
-      }
-      passport.authenticate('local')(req, res, function() {
-        res.redirect('/');
+
+    if(!usernameSchema.validate(req.body.username)) {
+      req.flash('error', 'Nazwa użytkownika zawiera niedozwolone znaki.');
+      res.redirect('/register');
+    }
+
+    else if(!passwordSchema.validate(req.body.password)) {
+      req.flash('error', 'Hasło musi składać się z kombinacji co najmniej 6 znaków w tym przynajmniej 1 litery i 1 cyfry.');
+      res.redirect('/register');
+    }
+
+    else {
+      var newUser = new User({
+        username: req.body.username
       });
-    });
+      User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+          console.log(err);
+          req.flash('error', 'Podana nazwa użytkownika istnieje.');
+          res.redirect('/register');
+        }
+        passport.authenticate('local')(req, res, function() {
+          res.redirect('/');
+        });
+      });
+    }
+
   } else {
     req.flash('error', 'Wpisałeś dwa różne hasła.');
     res.redirect('/register');
